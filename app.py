54b4c030c7a406406fd7cfe06ccb418048830eb8
Polaris Taki <zyyeh0120@gmail.com>
2020-07-06 23:31:46 +0800

    'ok'

--- app.py
+++ app.py
@@ -33,10 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    if 'n' in message:
-        message = TextSendMessage(text=event.message.text+"228922")
-        line_bot_api.reply_message(event.reply_token,message)
+    message = TextSendMessage(text=event.message.text+"228922")
+    line_bot_api.reply_message(event.reply_token,message)
 
 import os
 if __name__ == "__main__":
