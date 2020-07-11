7fee5ef42976a1806e3160aaca30f963cf39ef8e
Polaris Taki <zyyeh0120@gmail.com>
2020-07-12 02:34:51 +0800

    ok

--- Carouse.py
+++ Carouse.py
@@ -12,7 +12,7 @@ def image_carousel_message(link,input_message):
                     image_url=link,
                     action=URITemplateAction(
                         label=input_message,
-                        size=xl,
+                        
                     )
                 )
             ]
