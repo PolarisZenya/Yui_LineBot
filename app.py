a96743edf07c63ce94ad96a80936c3e944569f7f
Polaris Taki <zyyeh0120@gmail.com>
2020-07-12 02:26:16 +0800

    ok

--- app.py
+++ app.py
@@ -352,12 +352,14 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,message)
 # import Carouse.py
     elif input_message[0] == '我' and input_message[1] == '就':
-        if(i%3==1):
-            message = image_carousel_message('https://i.imgur.com/6oboN8l.jpg',input_message)
-        elif(i%3==2):
-            message = image_carousel_message('https://i.imgur.com/ZqjhK79.jpg',input_message)
-        elif(i%3==0):
-            message = image_carousel_message('https://i.imgur.com/nXsxbUW.jpg',input_message)
+        if(input_message[3] == '爛' and i%2==1):
+            message = ImageSendMessage(original_content_url = "https://i.imgur.com/ZqjhK79.jpg",preview_image_url = "https://i.imgur.com/ZqjhK79.jpg")
+        elif(input_message[3] == '爛' and i%2==0):
+            message = ImageSendMessage(original_content_url = "https://i.imgur.com/nXsxbUW.jpg",preview_image_url = "https://i.imgur.com/nXsxbUW.jpg")
+        elif(i%2==1):
+            message = image_carousel_message('https://i.imgur.com/avyrhK4.jpg',input_message)
+        elif(i%2==0):
+            message = image_carousel_message('https://i.imgur.com/qI0luB8.jpg',input_message)
         line_bot_api.reply_message(event.reply_token,message)
 # 動畫連結 import Animation.py
     elif input_message[0] == '#' and input_message[1] == '動' and input_message[2] == '畫':
