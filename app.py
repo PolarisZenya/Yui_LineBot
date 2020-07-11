70eee2a5327ecb7285921446e99d31a9275b73dd
Polaris Taki <zyyeh0120@gmail.com>
2020-07-11 15:04:44 +0800

    'ok'

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ import tempfile, os
 import datetime
 import time
 import webbrowser
-from pydub import AudioSegment
-import speech_recognition as sr
+#from pydub import AudioSegment
+#import speech_recognition as sr
 #================================
 
 app = Flask(__name__)
@@ -359,7 +359,7 @@ def handle_message(event):
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 # 創建時間2020/7/7
 
