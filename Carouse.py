e55f4083a52709944c427b9b5be0a6be86bd19db
Polaris Taki <zyyeh0120@gmail.com>
2020-07-12 04:13:38 +0800

    ok

--- Carouse.py
+++ Carouse.py
@@ -4,18 +4,66 @@ from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
 def image_carousel_message(link,input_message):
-    message = TemplateSendMessage(
-        alt_text='圖片旋轉木馬',
-        template=ImageCarouselTemplate(
-            columns=[
-                ImageCarouselColumn(
-                    image_url=link,
-                    action=MessageTemplateAction(
-                        label=input_message,
-                        text = '口黑口黑'
-                    )
-                )
-            ]
-        )
-    )
-    return message
+    line_bot_api = LineBotApi('発行されたCHANNEL_ACCESS_TOKEN')
+
+    payload = {
+        "type": "bubble",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "image",
+                    "url": "https://i.imgur.com/avyrhK4.jpg",
+                    "aspectMode": "cover",
+                    "size": "full"
+                },
+                {
+                    "type": "image",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip15.png",
+                    "position": "absolute",
+                    "aspectMode": "fit",
+                    "aspectRatio": "1:1",
+                    "offsetTop": "0px",
+                    "offsetBottom": "0px",
+                    "offsetStart": "0px",
+                    "offsetEnd": "0px",
+                    "size": "full"
+                },
+                {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "我就接頭霸王",
+                                            "size": "xl",
+                                            "color": "#ffffff"
+                                        }
+                                    ]
+                                }
+                            ],
+                            "spacing": "xs"
+                        }
+                    ],
+                    "offsetBottom": "0px",
+                    "offsetStart": "0px",
+                    "offsetEnd": "0px",
+                    "paddingAll": "20px",
+                    "position": "absolute"
+                }
+            ],
+            "paddingAll": "0px"
+        }
+    }
+
+    container_obj = FlexSendMessage.new_from_json_dict(payload)
+    return container_obj
