c7306eac2027bb7708e3c717186d654344d1dab0
Polaris Taki <zyyeh0120@gmail.com>
2020-07-16 02:29:01 +0800

    ok

--- Index.py
+++ Index.py
@@ -12,11 +12,8 @@ line_bot_api = LineBotApi('PpZXtWUOfOocv4On1fWAHOFUZEdJu6WNW/XPDBbppZ3/573sZ/eyv
 #============================================================
 def Judgment (i,input_message,event):
     if input_message == '#log':
-        line_bot_api.reply_message(
-            event.reply_token,TextSendMessage(
-                text="尻尻有益身體健康！拜託Line別再ban我了\n\n-nXXXXXX,wXXXXX就可以上車(e站也可試試)\n-公連角色梗、圖片支援！\n-輸入 #動畫+作品名稱 出現動畫連結！(開發中)\n\n*圖片畫質較高的回覆請耐心等待*\n*人家目前不支援簡體中文呦~*\n*詳細功能仍在開發中*\n*彩蛋很多放心吧~~*\n\n-已修復電腦版無法顯示圖片問題"
-                )
-            )
+        message = Log()
+        line_bot_api.reply_message(event.reply_token,message)
 # 發車
     elif '發車' in input_message or 'nhentai' in input_message or '老司機' in input_message or  input_message == '卡' or '色情' in input_message or '上車' in input_message:
         if(i%2==1):
