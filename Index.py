7d2fbe9f1ca2559e810846695b28932c9fba4c36
Polaris Taki <zyyeh0120@gmail.com>
2020-07-16 04:43:21 +0800

    'ok'

--- Index.py
+++ Index.py
@@ -421,11 +421,13 @@ def Judgment (i,input_message,event):
             message = ImageMessageURL("https://i.imgur.com/k1KEd3k.jpg")
             line_bot_api.reply_message(event.reply_token,[TextSendMessage(text='繪師twitter: @msr_mrk'),message])
     elif input_message == '可哥蘿':
-        if(i%2==1):
+        if(i%3==1):
             message = ImageMessageURL("https://i.imgur.com/gIF9vdY.png")
             line_bot_api.reply_message(event.reply_token,[TextSendMessage(text='是可可蘿啦...(可可蘿機器人哭倒路邊'),message])
-        elif(i%2==0):
+        elif(i%3==2):
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='真是的 騎士君又惹哭可哥蘿了...(咦?'))
+        elif(i%2==0):
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='可可萝同志乃我大美食殿堂不可分割之固有成员\n骑士君应充分理解和尊重美食殿堂的这一立场\n请立即正名可可萝'))
     elif input_message == '愛梅斯' or input_message == 'DD頭子' or input_message == 'アメス'  or input_message == '艾梅斯':
         if(i%9==1):
             message1 = ImageMessageURL("https://i.imgur.com/yk8dzMD.jpg")
