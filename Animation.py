54bea9eba34fd79827956162a157fbd935ed9239
Polaris Taki <zyyeh0120@gmail.com>
2020-07-18 22:00:03 +0800

    ok

--- Animation.py
+++ Animation.py
@@ -22,8 +22,12 @@ def ImageMessageURL (pic_url):
 
 # 動畫連結 import FlexMessage.py
 def Anime_View(input_message):
-    input_value = {
-        '工作細胞' : Anime_Return_abc(
+    if input_message == '#動畫':
+        return TextSendMessage(text ='不不不!!你搞錯了\n假設你要看re0動畫\n輸入: #動畫 re0\n即可~~')
+    elif '作品' in input_message:    
+        return TextSendMessage(text ='不不不!!你搞錯了\n假設你要看re0動畫\n輸入: #動畫 re0\n即可~~')
+    elif '工作細胞' in input_message:
+        return Anime_Return_abc(
             'https://ani.gamer.com.tw/animeVideo.php?sn=10210',
             'https://www.bilibili.com/bangumi/media/md102392',
             'https://abema.tv/video/title/26-53',
@@ -31,8 +35,9 @@ def Anime_View(input_message):
             'https://i.imgur.com/kPBFaz2.jpg',
             'https://i.imgur.com/d3oRiU7.jpg',
             'https://i.imgur.com/LbQJcj9.jpg'
-        ),
-        '鬼滅' or '鬼滅之刃' : Anime_Return_abc(
+        )
+    elif '鬼滅' in input_message:
+        return Anime_Return_abc(
             'https://ani.gamer.com.tw/animeVideo.php?sn=12083',
             'https://www.bilibili.com/bangumi/media/md25832466',
             'https://abema.tv/video/title/26-75',
@@ -40,15 +45,28 @@ def Anime_View(input_message):
             'https://i.imgur.com/Dk1Q6WI.jpg',
             'https://i.imgur.com/xGQLQ6c.jpg',
             'https://i.imgur.com/kxEmnj3.jpg'
-        ),
-        '公連' or '公主連結' : Anime_Return_bc(
+        )
+    elif '公連' in input_message or '公主連結' in input_message:
+        return Anime_Return_bc(
             'https://www.bilibili.com/bangumi/play/ss33095',
             'https://abema.tv/video/title/512-2',
             '超異域公主連結',
             'https://i.imgur.com/dqDTLAH.jpg',
             'https://i.imgur.com/B9lRrbU.jpg'
-        ),
-        're0' or 'Re0' or 'Re:0' : Anime_Return_abc(
+        )
+    elif ('re0' in input_message or 'Re0' in input_message or 'Re:0' in input_message) and '第二季' in input_message:
+        return Anime_Return_abc(
+            'https://ani.gamer.com.tw/animeVideo.php?sn=16344',
+            'https://www.bilibili.com/bangumi/media/md28229233',
+            'https://abema.tv/video/title/25-148',
+            'Re:Zero 第二季',
+            'https://i.imgur.com/dy5SWPI.jpg',
+            'https://i.imgur.com/TJ53X4g.jpg',
+            'https://i.imgur.com/am5ZzK5.jpg'
+            
+        )
+    elif 're0' in input_message or 'Re0' in input_message or 'Re:0' in input_message:
+        return Anime_Return_abc(
             'https://ani.gamer.com.tw/animeVideo.php?sn=14440',
             'https://www.bilibili.com/bangumi/media/md3461',
             'https://abema.tv/video/title/25-139',
@@ -56,8 +74,19 @@ def Anime_View(input_message):
             'https://i.imgur.com/fVkLdJV.jpg',
             'https://i.imgur.com/rQVZCGT.jpg',
             'https://i.imgur.com/qSI1tmA.jpg'
-        ),
-        '輝夜' or '輝夜姬' : Anime_Return_abc(
+        )
+    elif '輝夜' in input_message and '第二季' in input_message:
+        return Anime_Return_abc(
+            'https://ani.gamer.com.tw/animeVideo.php?sn=15298',
+            'https://www.bilibili.com/bangumi/media/md28228367',
+            'https://abema.tv/video/title/26-96',
+            '輝夜姬想讓人告白 第二季',
+            'https://i.imgur.com/ZS7xDXG.jpg',
+            'https://i.imgur.com/jTrXiqn.jpg',
+            'https://i.imgur.com/oHO6Axn.jpg'
+        )
+    elif '輝夜' in input_message:
+        return Anime_Return_abc(
             'https://ani.gamer.com.tw/animeVideo.php?sn=11431',
             'https://www.bilibili.com/bangumi/media/md5267730',
             'https://abema.tv/video/title/26-66',
@@ -66,7 +95,3 @@ def Anime_View(input_message):
             'https://i.imgur.com/oiyKEI8.jpg',
             'https://i.imgur.com/XalMrNf.jpg'
         )
-    }
-    return input_value[input_message]
-
-    
