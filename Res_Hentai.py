66fb9a9cb7431cc9b78b56db969b2ee86b62446f
Polaris Taki <zyyeh0120@gmail.com>
2020-08-03 14:07:50 +0800

    ok

--- Res_Hentai.py
+++ Res_Hentai.py
@@ -15,15 +15,12 @@ def getData(Action_but,url,user_input):
     with req.urlopen(request) as response:
         data = response.read().decode("utf-8")
     soup = bs4.BeautifulSoup(data, "html.parser")
-    try:
-        if "» nhentai: hentai doujinshi and manga" in soup.title.string :
-            Title=str(soup.title.string).split("» nhentai: hentai doujinshi and manga")[0]
-            PicURL = soup.find("meta", itemprop="image")
-            PicURL = str(PicURL).replace('<meta content="','')
-            PicURL = PicURL.replace('" itemprop="image"/>','')
-            return Hentai_Path(Action_but,URL,PicURL,Title,user_input)
-        elif "- 列表 - 紳士漫畫-專註分享漢化本子|邪惡漫畫" in soup.title.string :
-            print(str(soup.title.string).split("- 列表 - 紳士漫畫-專註分享漢化本子|邪惡漫畫")[0])
-    except:
-        return TextSendMessage(text = "404 not found"))
+    if "» nhentai: hentai doujinshi and manga" in soup.title.string :
+        Title=str(soup.title.string).split("» nhentai: hentai doujinshi and manga")[0]
+        PicURL = soup.find("meta", itemprop="image")
+        PicURL = str(PicURL).replace('<meta content="','')
+        PicURL = PicURL.replace('" itemprop="image"/>','')
+        return Hentai_Path(Action_but,URL,PicURL,Title,user_input)
+    elif "- 列表 - 紳士漫畫-專註分享漢化本子|邪惡漫畫" in soup.title.string :
+        print(str(soup.title.string).split("- 列表 - 紳士漫畫-專註分享漢化本子|邪惡漫畫")[0])
 #url = "http://wnacg.org/photos-slide-aid-100000.html"
