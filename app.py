28c0b283135c1c3570a75c63f1278ca09633a19c
Polaris Taki <zyyeh0120@gmail.com>
2020-08-03 10:03:58 +0800

    ok

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def handle_join(event):
 # follow 
 @handler.add(FollowEvent)
 def handle_Follow(event):
-    Follow_text = "我來到一個新世界了嗎\n這個世界應該不再充斥著背叛了吧?\n對不起學姊\n這次，我...\n是永遠屬於各位騎士君的那個優衣呦\n*人家目前不支援簡體中文呦~*"
+    Follow_text = "騎士君歡迎！\n\n車車、圖圖、meme為這個世界帶來希望與和平，也可以把人家拉入群組\n輸入 #log 或是點選下面選單可察看功能~~\n頭貼圖源: twitter@nohhun144"
     message = Log()
     line_bot_api.reply_message(event.reply_token,[TextMessage(text=Follow_text),message])
 # leave
