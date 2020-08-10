5f6b1451d4d71b3e65d1a9997157b5f34417c05a
Polaris Taki <zyyeh0120@gmail.com>
2020-08-11 05:52:32 +0800

    ok

--- Index.py
+++ Index.py
@@ -1724,7 +1724,7 @@ def Judgment (i,input_message,event):
     elif input_message[0] in 'Nn' and input_message[1] in '1234567890' and len(input_message) <= 7 :
         num =''.join([x for x in input_message if x.isdigit()])
         if eval(num)==0 :
-            num = str(267232+i*32)
+            num = str(267231+i*32)
         elif((eval(num))==228922 or (eval(num))==173156 or (eval(num))==196970) :
             value_i = {
                 1 : "等等...騎士君，別告訴我你是認真的",
