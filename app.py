f5e3f809580230deacad631d1f684e18da2932ea
Polaris Taki <zyyeh0120@gmail.com>
2020-10-11 03:47:45 +0800

    'ok'

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-#============================================================
+#=============================================================
 # welcome
 @handler.add(JoinEvent)
 def handle_join(event):
