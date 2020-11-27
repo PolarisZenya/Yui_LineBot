c0da1635ce71614a110034aa1b356b3eae918936
Polaris Taki <zyyeh0120@gmail.com>
2020-11-28 02:58:34 +0800

    'ok'

--- app.py
+++ app.py
@@ -58,11 +58,11 @@ def handle_message(event):
     except:
         pass
 #獵巫(私密聊天)
-#    if (user=="U770bbc6dc15278742deaec9399644742") and input_message[:2] == '>>': 
-#        print("訊息已經成功寄出")
-#        line_bot_api.push_message('Ue41f9234f4bc353cb50b36f36c91b50e', TextSendMessage(text= input_message.replace(">>","") ))
-#        line_bot_api.reply_message(event.reply_token,TextSendMessage(text= "已發送 "+input_message.replace(">>","")))
-#        return
+    if (user=="U770bbc6dc15278742deaec9399644742") and input_message[:2] == '>>': 
+        print("訊息已經成功寄出")
+        line_bot_api.push_message('Ue41f9234f4bc353cb50b36f36c91b50e', TextSendMessage(text= input_message.replace(">>","") ))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text= "已發送 "+input_message.replace(">>","")))
+        return
 
     JUD = Index_Judgment()
     try:
