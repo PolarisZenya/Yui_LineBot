095c7e6e35a10b7df6de362637e602b000704d62
Polaris Taki <zyyeh0120@gmail.com>
2021-02-11 05:03:02 +0800

    '全域變數佔存'

--- Globals.py
+++ Globals.py
@@ -1,6 +1,5 @@
 from Insert_Sheet_Data import *
 #============================================================
-
 Yui_denied_group  = Google_Sheet_DataBase().Data_Get_group()
 Yui_denied_access = Google_Sheet_DataBase().Data_Get_accesslvl()
 
