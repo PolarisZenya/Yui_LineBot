f48de1afced3a38114f911dab3f4ed9efb2c0add
Polaris Taki <zyyeh0120@gmail.com>
2021-02-11 05:08:31 +0800

    'Flask.site.py'

--- app.py
+++ app.py
@@ -17,15 +17,11 @@ handler = WebhookHandler('ENTER_YOUR_LINEBOT_CLIENT_SECRET')
 #============================================================
 Globals
 #============================================================
-# /callback  Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-# get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-# get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-# handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
